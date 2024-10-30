b0ca6c9de5e4e220414b517ca188fcf32337e314
Root User <admin@demo.com>
2024-10-30 21:43:33 +0530

    Latest updates

--- index.php
+++ index.php
@@ -7,20 +7,12 @@ if(!isset($_REQUEST['panel'])) $_REQUEST['panel']="import";
 
 $pageOptions=[
 		"toolbar"=>[
-		    //["title"=>"Search Store","type"=>"search","align"=>"right"],
-            
-			//"verifier"=>["title"=>"Verifier","align"=>"right","class"=>($_REQUEST['panel']=="verifier")?"active":""],
-			"importPane"=>["title"=>"Import","align"=>"right","class"=>($_REQUEST['panel']=="import")?"active":""],
-      "exportPane"=>["title"=>"Export","align"=>"right","class"=>($_REQUEST['panel']=="export")?"active":""],
-      "migratePane"=>["title"=>"Migrate","align"=>"right","class"=>($_REQUEST['panel']=="migrate")?"active":""],
+		    "importPane"=>["title"=>"Import","align"=>"right","class"=>($_REQUEST['panel']=="import")?"active":""],
+            "exportPane"=>["title"=>"Export","align"=>"right","class"=>($_REQUEST['panel']=="export")?"active":""],
+            // "migratePane"=>["title"=>"Migrate","align"=>"right","class"=>($_REQUEST['panel']=="migrate")?"active":""],
 			// ['type'=>"bar"],
             
-      "refreshUI"=>["icon"=>"<i class='fa fa-refresh'></i>"],
-			
-			
-// 			"createNew"=>["icon"=>"<i class='fa fa-plus'></i>","tips"=>"Create New"],
-// 			['type'=>"bar"],
-// 			"trash"=>["icon"=>"<i class='fa fa-trash'></i>"],
+            "refreshUI"=>["icon"=>"<i class='fa fa-refresh'></i>"],
 		],
 		"contentArea"=>"pageContentArea"
 	];
--- panels/migrate.php
+++ panels/migrate.php
@@ -1,1 +1,2 @@
-migrate
+<br><br><br>
+<h3 align=center>Coming Soon ....</h3>
